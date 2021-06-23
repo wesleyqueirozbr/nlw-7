ded858673c1a94dc4ec2a650e9d003792375809f
Wesley Queiroz <84075020+wesleyqueirozbr@users.noreply.github.com>
2021-06-23 04:28:23 -0300

    AULA 03

--- main.js
+++ main.js
@@ -1,18 +1,18 @@
 // ABRE E FECHA O MEU QUANDO CLICAR NO ICONE
-const nav = document.querySelector("#header nav")
-const toogle = document.querySelectorAll("nav .toggle")
+const nav = document.querySelector('#header nav')
+const toogle = document.querySelectorAll('nav .toggle')
 
-for(const element of toogle) {
-  element.addEventListener("click",function() {
-    nav.classList.toggle("show")
+for (const element of toogle) {
+  element.addEventListener('click', function () {
+    nav.classList.toggle('show')
   })
 }
 
 // QUANDO CLICAR EM UM ITEM DO MENU, ESCONDE O MEU
 
-const links = document.querySelectorAll("nav ul li a")
+const links = document.querySelectorAll('nav ul li a')
 
-for(const link of links) {
+for (const link of links) {
   link.addEventListener('click', function () {
     nav.classList.remove('show')
   })
@@ -29,3 +29,30 @@ window.addEventListener('scroll', function () {
     header.classList.remove('scroll')
   }
 })
+
+const swiper = new Swiper('.swiper-container', {
+  slidesPerView: 1,
+  pagination: {
+    el: '.swiper-pagination'
+  },
+  mousewheel: true,
+  keyboard: true
+})
+
+// ScrollReveal: Mostrar elementos quando der scroll na página
+
+const scrollReveal = ScrollReveal({
+  origin: 'top',
+  distance: '30px',
+  duration: 700,
+  reset: true
+})
+scrollReveal.reveal(
+  `#home .image, #home .text,
+  #about .image, #about .text,
+  #services header, #services .card,
+  #testimonials header,#testimonials .testimonials
+  #contact .text, #contact .links
+`,
+  { interval: 100 }
+)
